aec1ac4ce2e28504dc3559eac1d0fbdc983466d6
phoenix-d0wn <hotnoodlebroth@proton.me>
2026-08-04 14:40:34 +0800

    .

--- examples/remind.c
+++ examples/remind.c
@@ -2,73 +2,63 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-#define MSG_LEN     60
-#define DAY_LEN     2
-#define MAX_REMINDS 50
+#define MSG_LEN         4
+#define DAY_LEN         2
+#define MAX_REMINDERS   4
 
-char remind[MAX_REMINDS][DAY_LEN + MSG_LEN + 1];
+char reminders[MAX_REMINDERS][DAY_LEN + MSG_LEN + 1];
 
-int read_msg(char msg_str[]);
+void read_day(char day_str[]);
+void read_msg(char msg[]);
+void ins_reminder(char day_str[], char msg[], int num_reminders);
 
-int main(void)
+int main(void) 
 {
-    int num_reminds = 0;
-    int day;
-    char day_str[DAY_LEN+1], msg_str[MSG_LEN + 1];
-    int i, j;
+    char day_str[DAY_LEN+1], msg[MSG_LEN+1];
+    
+    printf("Enter day and reminder: ");
+    read_day(day_str);
+    read_msg(msg);
 
+    //TODO comment out tests
+    //test day_str[]
+    printf("TEST: day_str = %s\n", day_str);
+    //test msg[]
+    printf("TEST: msg = %s\n", msg);
 
-
-    for (;;) {
-        
-        if (num_reminds >= MAX_REMINDS) {
-            printf("--Max reminders reached!--");   
-            break;
-        }
-
-        // printf("****TEST******\n");
-        printf("Enter day and reminder: ");
-        scanf("%d", &day);
-
-        if (day == 0)   break;      //stop entering reminders
-
-        sprintf(day_str, "%2d", day);
-        read_msg(msg_str);
-
-        for (i = 0; (strcmp(day_str, remind[i]) > 0) && (i < num_reminds); i++)
-            // printf("i = %d\n", i);
-        for (j = num_reminds; j > i; j--) {
-            strcpy(remind[j], remind[j - 1]);
-            // printf("j = %d\n", j);
-        }
-
-        strcpy(remind[i], day_str);
-        strcat(remind[i], msg_str);
-
-        num_reminds++;
-    }
-
-    printf("\nDay Reminder\n");
-    for (i = 0; i < num_reminds; i++)
-        printf("%s\n", remind[i]);
-
+    // //test strcmp()
+    // char a[] = "25", b[] = "5 dfgwdgdrsgs";
+    // printf("a < b ? : %d\n", strcmp(b, a) > 0);
 
 
     return 0;
 }
 
-int read_msg(char msg_str[])
+void read_day(char day_str[])
 {
-    char ch;
+    int day;
+    scanf("%2d", &day);
+    sprintf(day_str, "%2d", day);
+}
+
+void read_msg(char msg[])
+{
     int i;
+    char ch;
 
-    for (i = 0; ((ch = getchar()) != '\n') && (i < MSG_LEN); i++) {
-
-        msg_str[i] = ch;
+    for (i = 0; ((ch = getchar()) != '\n') && (i <= MSG_LEN); i++) {
+        msg[i] = ch;
     }
-    msg_str[i] = '\0';
-    
-    return i;
+    msg[i] = '\0';
+}
+
+//ins_reminder(): insert reminder in ascending order (by day #)
+void ins_reminder(char day_str[], char msg[], int num_reminders)
+{
+    int i, j;
+    for (i = 0; (i < num_reminders) && (strcmp(day_str, reminders[i]) <= 0); i++)
+        ;
+    printf("i = %d\n", i);
+
 }
